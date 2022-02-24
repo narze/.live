94076a14665699b900f313566127117b71e60a34
Manassarn Manoonchai <manassarn@gmail.com>
2022-02-24 21:40:53 +0700

    Wave on hover

--- src/components/Greeting.svelte
+++ src/components/Greeting.svelte
@@ -1,4 +1,11 @@
 <h1 class="text-5xl md:text-6xl flex flex-col md:flex-row justify-center gap-2">
   <span>Hello there, </span>
-  <span>I'm <span class="font-bold name">narze</span> 👋</span>
+  <span
+    >I'm <span class="font-bold name">narze</span>
+    <div
+      class="inline-block transition hover:rotate-45 ease-in-out duration-300"
+    >
+      👋
+    </div></span
+  >
 </h1>
