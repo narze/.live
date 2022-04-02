3f7c46113153095d51cd2eb6cc0ffde5f99ea109
Manassarn Manoonchai <manassarn@gmail.com>
2022-04-02 11:27:10 +0700

    feat: add twitch link

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -4,14 +4,16 @@
   import YoutubeIcon from "./YoutubeIcon.svelte"
   import NoteIcon from "./NoteIcon.svelte"
   import BriefcaseIcon from "./BriefcaseIcon.svelte"
+  import TwitchIcon from "./TwitchIcon.svelte"
 </script>
 
 <div class="flex flex-col xs:flex-row flex-wrap gap-4 justify-center mt-8">
   <LinkButton href="https://garden.narze.live"><NoteIcon />Garden</LinkButton>
   <LinkButton href="https://github.com/narze"><CodeIcon />GitHub</LinkButton>
-  <LinkButton href="https://www.youtube.com/c/narzelive"
+  <LinkButton href="https://youtube.narze.live"
     ><YoutubeIcon />YouTube</LinkButton
   >
+  <LinkButton href="https://twitch.narze.live"><TwitchIcon />Twitch</LinkButton>
   <LinkButton href="https://resume.narze.live"
     ><BriefcaseIcon />Résumé</LinkButton
   >
