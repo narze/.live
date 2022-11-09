b65aec55f0cc9b8d0d2c6f0b438f82b4e85df1be
Manassarn "Noom" Manoonchai <248741+narze@users.noreply.github.com>
2022-11-09 18:37:33 +0700

    See you next year!

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -9,9 +9,6 @@
 </script>
 
 <div class="flex flex-col xs:flex-row flex-wrap gap-4 justify-center mt-8">
-  <LinkButton href="https://hacktober.narze.live"
-    ><HacktoberFestIcon /><span class="name">Hacktoberfest</span></LinkButton
-  >
   <LinkButton href="https://garden.narze.live"><NoteIcon />Garden</LinkButton>
   <LinkButton href="https://github.com/narze"><CodeIcon />GitHub</LinkButton>
   <LinkButton href="https://youtube.narze.live"
