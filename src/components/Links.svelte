5c03be5473ee61c27a0f005c3a405669a89f8a94
Manassarn Manoonchai <manassarn@gmail.com>
2023-09-02 21:26:31 +0700

    Add 10k planner link

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -9,15 +9,23 @@
   import HacktoberFestIcon from "./HacktoberFestIcon.svelte"
 </script>
 
-<div class="flex flex-col xs:flex-row flex-wrap gap-4 justify-center mt-8">
-  <LinkButton href="https://garden.narze.live"><NoteIcon />Garden</LinkButton>
-  <LinkButton href="https://chat.narze.live"><ChatIcon />ChatOS</LinkButton>
-  <LinkButton href="https://github.com/narze"><CodeIcon />GitHub</LinkButton>
-  <LinkButton href="https://youtube.narze.live"
-    ><YoutubeIcon />YouTube</LinkButton
-  >
-  <LinkButton href="https://twitch.narze.live"><TwitchIcon />Twitch</LinkButton>
-  <LinkButton href="https://resume.narze.live"
-    ><BriefcaseIcon />Résumé</LinkButton
-  >
+<div class="flex flex-col gap-4">
+  <div class="flex flex-col xs:flex-row flex-wrap gap-4 justify-center mt-8">
+    <LinkButton href="https://10k.narze.live">💸 10k Planner (New!)</LinkButton>
+    <LinkButton href="https://garden.narze.live"><NoteIcon />Garden</LinkButton>
+    <LinkButton href="https://chat.narze.live"><ChatIcon />ChatOS</LinkButton>
+  </div>
+
+  <div class="flex flex-col xs:flex-row flex-wrap gap-4 justify-center">
+    <LinkButton href="https://github.com/narze"><CodeIcon />GitHub</LinkButton>
+    <LinkButton href="https://youtube.narze.live"
+      ><YoutubeIcon />YouTube</LinkButton
+    >
+    <LinkButton href="https://twitch.narze.live"
+      ><TwitchIcon />Twitch</LinkButton
+    >
+    <LinkButton href="https://resume.narze.live"
+      ><BriefcaseIcon />Résumé</LinkButton
+    >
+  </div>
 </div>
